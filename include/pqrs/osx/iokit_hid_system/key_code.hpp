62318b6192d3118f28393ab7c49e275c5b41dd90
Takayama Fumihiko <tekezo@pqrs.org>
2020-04-10 22:02:18 +0900

    update usage_page_keyboard_or_keypad_key_code_pairs

--- include/pqrs/osx/iokit_hid_system/key_code.hpp
+++ include/pqrs/osx/iokit_hid_system/key_code.hpp
@@ -45,7 +45,6 @@ constexpr std::pair<type_safe::underlying_type<iokit_hid_usage>, key_code> usage
     {type_safe::get(iokit_hid_usage_keyboard_x), key_code(0x7)},
     {type_safe::get(iokit_hid_usage_keyboard_y), key_code(0x10)},
     {type_safe::get(iokit_hid_usage_keyboard_z), key_code(0x6)},
-
     {type_safe::get(iokit_hid_usage_keyboard_1), key_code(0x12)},
     {type_safe::get(iokit_hid_usage_keyboard_2), key_code(0x13)},
     {type_safe::get(iokit_hid_usage_keyboard_3), key_code(0x14)},
@@ -56,7 +55,6 @@ constexpr std::pair<type_safe::underlying_type<iokit_hid_usage>, key_code> usage
     {type_safe::get(iokit_hid_usage_keyboard_8), key_code(0x1c)},
     {type_safe::get(iokit_hid_usage_keyboard_9), key_code(0x19)},
     {type_safe::get(iokit_hid_usage_keyboard_0), key_code(0x1d)},
-
     {type_safe::get(iokit_hid_usage_keyboard_return_or_enter), key_code(0x24)},
     {type_safe::get(iokit_hid_usage_keyboard_escape), key_code(0x35)},
     {type_safe::get(iokit_hid_usage_keyboard_delete_or_backspace), key_code(0x33)},
@@ -67,8 +65,7 @@ constexpr std::pair<type_safe::underlying_type<iokit_hid_usage>, key_code> usage
     {type_safe::get(iokit_hid_usage_keyboard_open_bracket), key_code(0x21)},
     {type_safe::get(iokit_hid_usage_keyboard_close_bracket), key_code(0x1e)},
     {type_safe::get(iokit_hid_usage_keyboard_backslash), key_code(0x2a)},
-    // backslash ==  non_us_pound
-    {type_safe::get(iokit_hid_usage_keyboard_non_us_pound), key_code(0x2a)},
+    {type_safe::get(iokit_hid_usage_keyboard_non_us_pound), key_code(0x2a)}, // non_us_pound == backslash
     {type_safe::get(iokit_hid_usage_keyboard_semicolon), key_code(0x29)},
     {type_safe::get(iokit_hid_usage_keyboard_quote), key_code(0x27)},
     {type_safe::get(iokit_hid_usage_keyboard_grave_accent_and_tilde), key_code(0x32)},
@@ -76,7 +73,6 @@ constexpr std::pair<type_safe::underlying_type<iokit_hid_usage>, key_code> usage
     {type_safe::get(iokit_hid_usage_keyboard_period), key_code(0x2f)},
     {type_safe::get(iokit_hid_usage_keyboard_slash), key_code(0x2c)},
     {type_safe::get(iokit_hid_usage_keyboard_caps_lock), key_code(0x39)},
-
     {type_safe::get(iokit_hid_usage_keyboard_f1), key_code(0x7a)},
     {type_safe::get(iokit_hid_usage_keyboard_f2), key_code(0x78)},
     {type_safe::get(iokit_hid_usage_keyboard_f3), key_code(0x63)},
@@ -89,6 +85,110 @@ constexpr std::pair<type_safe::underlying_type<iokit_hid_usage>, key_code> usage
     {type_safe::get(iokit_hid_usage_keyboard_f10), key_code(0x6d)},
     {type_safe::get(iokit_hid_usage_keyboard_f11), key_code(0x67)},
     {type_safe::get(iokit_hid_usage_keyboard_f12), key_code(0x6f)},
+    {type_safe::get(iokit_hid_usage_keyboard_print_screen), key_code(0x69)},
+    {type_safe::get(iokit_hid_usage_keyboard_scroll_lock), key_code(0x6b)},
+    {type_safe::get(iokit_hid_usage_keyboard_pause), key_code(0x71)},
+    {type_safe::get(iokit_hid_usage_keyboard_insert), key_code(0x72)},
+    {type_safe::get(iokit_hid_usage_keyboard_home), key_code(0x73)},
+    {type_safe::get(iokit_hid_usage_keyboard_page_up), key_code(0x74)},
+    {type_safe::get(iokit_hid_usage_keyboard_delete_forward), key_code(0x75)},
+    {type_safe::get(iokit_hid_usage_keyboard_end), key_code(0x77)},
+    {type_safe::get(iokit_hid_usage_keyboard_page_down), key_code(0x79)},
+    {type_safe::get(iokit_hid_usage_keyboard_right_arrow), key_code(0x7c)},
+    {type_safe::get(iokit_hid_usage_keyboard_left_arrow), key_code(0x7b)},
+    {type_safe::get(iokit_hid_usage_keyboard_down_arrow), key_code(0x7d)},
+    {type_safe::get(iokit_hid_usage_keyboard_up_arrow), key_code(0x7e)},
+    {type_safe::get(iokit_hid_usage_keypad_num_lock), key_code(0x47)},
+    {type_safe::get(iokit_hid_usage_keypad_slash), key_code(0x4b)},
+    {type_safe::get(iokit_hid_usage_keypad_asterisk), key_code(0x43)},
+    {type_safe::get(iokit_hid_usage_keypad_hyphen), key_code(0x4e)},
+    {type_safe::get(iokit_hid_usage_keypad_plus), key_code(0x45)},
+    {type_safe::get(iokit_hid_usage_keypad_enter), key_code(0x4c)},
+    {type_safe::get(iokit_hid_usage_keypad_1), key_code(0x53)},
+    {type_safe::get(iokit_hid_usage_keypad_2), key_code(0x54)},
+    {type_safe::get(iokit_hid_usage_keypad_3), key_code(0x55)},
+    {type_safe::get(iokit_hid_usage_keypad_4), key_code(0x56)},
+    {type_safe::get(iokit_hid_usage_keypad_5), key_code(0x57)},
+    {type_safe::get(iokit_hid_usage_keypad_6), key_code(0x58)},
+    {type_safe::get(iokit_hid_usage_keypad_7), key_code(0x59)},
+    {type_safe::get(iokit_hid_usage_keypad_8), key_code(0x5b)},
+    {type_safe::get(iokit_hid_usage_keypad_9), key_code(0x5c)},
+    {type_safe::get(iokit_hid_usage_keypad_0), key_code(0x52)},
+    {type_safe::get(iokit_hid_usage_keypad_period), key_code(0x41)},
+    {type_safe::get(iokit_hid_usage_keyboard_non_us_backslash), key_code(0xa)},
+    {type_safe::get(iokit_hid_usage_keyboard_application), key_code(0x6e)},
+    // iokit_hid_usage_keyboard_power => get_hid_aux_control_button_map
+    {type_safe::get(iokit_hid_usage_keypad_equal_sign), key_code(0x51)},
+    {type_safe::get(iokit_hid_usage_keyboard_f13), key_code(0x69)},
+    {type_safe::get(iokit_hid_usage_keyboard_f14), key_code(0x6b)},
+    {type_safe::get(iokit_hid_usage_keyboard_f15), key_code(0x71)},
+    {type_safe::get(iokit_hid_usage_keyboard_f16), key_code(0x6a)},
+    {type_safe::get(iokit_hid_usage_keyboard_f17), key_code(0x40)},
+    {type_safe::get(iokit_hid_usage_keyboard_f18), key_code(0x4f)},
+    {type_safe::get(iokit_hid_usage_keyboard_f19), key_code(0x50)},
+    {type_safe::get(iokit_hid_usage_keyboard_f20), key_code(0x5a)},
+    // iokit_hid_usage_keyboard_f21
+    // iokit_hid_usage_keyboard_f22
+    // iokit_hid_usage_keyboard_f23
+    // iokit_hid_usage_keyboard_f24
+    // iokit_hid_usage_keyboard_execute
+    {type_safe::get(iokit_hid_usage_keyboard_help), key_code(0x72)},
+    // iokit_hid_usage_keyboard_menu
+    // iokit_hid_usage_keyboard_select
+    // iokit_hid_usage_keyboard_stop
+    // iokit_hid_usage_keyboard_again
+    // iokit_hid_usage_keyboard_undo
+    // iokit_hid_usage_keyboard_cut
+    // iokit_hid_usage_keyboard_copy
+    // iokit_hid_usage_keyboard_paste
+    // iokit_hid_usage_keyboard_find
+    // iokit_hid_usage_keyboard_mute => get_hid_aux_control_button_map
+    // iokit_hid_usage_keyboard_volume_up => get_hid_aux_control_button_map
+    // iokit_hid_usage_keyboard_volume_down => get_hid_aux_control_button_map
+    // iokit_hid_usage_keyboard_locking_caps_lock
+    // iokit_hid_usage_keyboard_locking_num_lock
+    // iokit_hid_usage_keyboard_locking_scroll_lock
+    {type_safe::get(iokit_hid_usage_keypad_comma), key_code(0x5f)},
+    // iokit_hid_usage_keypad_equal_sign_as400
+    {type_safe::get(iokit_hid_usage_keyboard_international1), key_code(0x5e)},
+    // iokit_hid_usage_keyboard_international2
+    {type_safe::get(iokit_hid_usage_keyboard_international3), key_code(0x5d)},
+    // iokit_hid_usage_keyboard_international4
+    // iokit_hid_usage_keyboard_international5
+    // iokit_hid_usage_keyboard_international6
+    // iokit_hid_usage_keyboard_international7
+    // iokit_hid_usage_keyboard_international8
+    // iokit_hid_usage_keyboard_international9
+    {type_safe::get(iokit_hid_usage_keyboard_lang1), key_code(0x68)},
+    {type_safe::get(iokit_hid_usage_keyboard_lang2), key_code(0x66)},
+    // iokit_hid_usage_keyboard_lang3
+    // iokit_hid_usage_keyboard_lang4
+    // iokit_hid_usage_keyboard_lang5
+    // iokit_hid_usage_keyboard_lang6
+    // iokit_hid_usage_keyboard_lang7
+    // iokit_hid_usage_keyboard_lang8
+    // iokit_hid_usage_keyboard_lang9
+    // iokit_hid_usage_keyboard_alternate_erase
+    // iokit_hid_usage_keyboard_sys_req_or_attention
+    // iokit_hid_usage_keyboard_cancel
+    // iokit_hid_usage_keyboard_clear
+    // iokit_hid_usage_keyboard_prior
+    // iokit_hid_usage_keyboard_return
+    // iokit_hid_usage_keyboard_separator
+    // iokit_hid_usage_keyboard_out
+    // iokit_hid_usage_keyboard_oper
+    // iokit_hid_usage_keyboard_clear_or_again
+    // iokit_hid_usage_keyboard_cr_sel_or_props
+    // iokit_hid_usage_keyboard_ex_sel
+
+    {type_safe::get(iokit_hid_usage_keyboard_left_control), key_code(0x3b)},
+    {type_safe::get(iokit_hid_usage_keyboard_left_shift), key_code(0x38)},
+    {type_safe::get(iokit_hid_usage_keyboard_left_alt), key_code(0x3a)},
+    {type_safe::get(iokit_hid_usage_keyboard_left_gui), key_code(0x37)},
+    {type_safe::get(iokit_hid_usage_keyboard_right_control), key_code(0x3e)},
+    {type_safe::get(iokit_hid_usage_keyboard_right_shift), key_code(0x3c)},
+    {type_safe::get(iokit_hid_usage_keyboard_right_alt), key_code(0x3d)},
+    {type_safe::get(iokit_hid_usage_keyboard_right_gui), key_code(0x36)},
 };
 constexpr auto usage_page_keyboard_or_keypad_key_code_map = frozen::make_unordered_map(usage_page_keyboard_or_keypad_key_code_pairs);
 
