5d942acc8a1fc3cd67159ef58b3e9f2be75e8c84
Takayama Fumihiko <tekezo@pqrs.org>
2020-04-13 22:04:52 +0900

    add async_post_key_code_event

--- include/pqrs/osx/iokit_hid_system/client.hpp
+++ include/pqrs/osx/iokit_hid_system/client.hpp
@@ -5,11 +5,12 @@
 // (See http://www.boost.org/LICENSE_1_0.txt)
 
 #include "aux_control_button.hpp"
+#include "event_type.hpp"
 #include "key_code.hpp"
 #include <IOKit/hidsystem/IOHIDParameter.h>
 #include <IOKit/hidsystem/IOHIDShared.h>
-#include <pqrs/osx/iokit_return.hpp>
 #include <pqrs/osx/iokit_service_monitor.hpp>
+#include <pqrs/osx/kern_return.hpp>
 
 // Note:
 // OS X shares IOHIDSystem among all input devices even the serial_number of IOHIDSystem is same with the one of the input device.
@@ -31,14 +32,17 @@ class client final : pqrs::dispatcher::extra::dispatcher_client {
 public:
   // Signals (invoked from the shared dispatcher thread)
 
+  nod::signal<void(void)> opened;
+  nod::signal<void(void)> closed;
+  nod::signal<void(const std::string&, kern_return)> error_occurred;
   nod::signal<void(std::optional<bool>)> caps_lock_state_changed;
 
   // Methods
 
   client(const client&) = delete;
 
-  client(void) : dispatcher_client(),
-                 caps_lock_state_check_timer_(*this) {
+  client(std::weak_ptr<dispatcher::dispatcher> weak_dispatcher) : dispatcher_client(weak_dispatcher),
+                                                                  caps_lock_state_check_timer_(*this) {
     if (auto matching_dictionary = IOServiceNameMatching(kIOHIDSystemClass)) {
       service_monitor_ = std::make_unique<iokit_service_monitor>(weak_dispatcher_,
                                                                  matching_dictionary);
@@ -57,8 +61,6 @@ public:
         service_monitor_->async_invoke_service_matched();
       });
 
-      service_monitor_->async_start();
-
       CFRelease(matching_dictionary);
     }
   }
@@ -69,6 +71,12 @@ public:
       close_connection();
 
       service_monitor_ = nullptr;
+    });
+  }
+
+  void async_start(void) {
+    enqueue_to_dispatcher([this] {
+      service_monitor_->async_start();
     });
   }
 
@@ -96,6 +104,55 @@ public:
     });
   }
 
+  void async_post_key_code_event(event_type::value_t event_type, key_code::value_t key_code, IOOptionBits flags, bool repeat) {
+    NXEventData event{};
+    event.key.origCharCode = 0;
+    event.key.repeat = repeat;
+    event.key.charSet = NX_ASCIISET;
+    event.key.charCode = 0;
+    event.key.keyCode = type_safe::get(key_code);
+    event.key.origCharSet = NX_ASCIISET;
+    event.key.keyboardType = 0;
+
+    IOGPoint loc{};
+    async_post_event(event_type,
+                     loc,
+                     event,
+                     kNXEventDataVersion,
+                     flags,
+                     kIOHIDSetGlobalEventFlags);
+  }
+
+  void async_post_event(event_type::value_t event_type,
+                        IOGPoint location,
+                        const NXEventData& event_data,
+                        UInt32 event_data_version,
+                        IOOptionBits event_flags,
+                        IOOptionBits options) {
+    enqueue_to_dispatcher([this,
+                           event_type,
+                           location,
+                           event_data,
+                           event_data_version,
+                           event_flags,
+                           options] {
+      if (connect_) {
+        kern_return r = IOHIDPostEvent(connect_,
+                                       type_safe::get(event_type),
+                                       location,
+                                       &event_data,
+                                       event_data_version,
+                                       event_flags,
+                                       options);
+        if (!r) {
+          enqueue_to_dispatcher([this, r] {
+            error_occurred("IOHIDPostEvent is failed.", r);
+          });
+        }
+      }
+    });
+  }
+
 private:
   // This method is executed in the dispatcher thread.
   void open_connection(iokit_object_ptr s) {
@@ -103,9 +160,17 @@ private:
       service_ = s;
 
       io_connect_t c;
-      iokit_return r = IOServiceOpen(*service_, mach_task_self(), kIOHIDParamConnectType, &c);
+      kern_return r = IOServiceOpen(*service_, mach_task_self(), kIOHIDParamConnectType, &c);
       if (r) {
         connect_ = iokit_object_ptr(c);
+
+        enqueue_to_dispatcher([this] {
+          opened();
+        });
+      } else {
+        enqueue_to_dispatcher([this, r] {
+          error_occurred("IOServiceOpen is failed.", r);
+        });
       }
     }
   }
@@ -115,6 +180,10 @@ private:
     if (connect_) {
       IOServiceClose(*connect_);
       connect_.reset();
+
+      enqueue_to_dispatcher([this] {
+        closed();
+      });
     }
 
     service_.reset();
@@ -127,7 +196,7 @@ private:
     }
 
     bool state = false;
-    iokit_return r = IOHIDGetModifierLockState(*connect_, selector, &state);
+    kern_return r = IOHIDGetModifierLockState(*connect_, selector, &state);
     if (!r) {
       return std::nullopt;
     }
